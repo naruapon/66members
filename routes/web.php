3cabbd80b8127a4cda77097329692e584f56c30c
Naruapon Suwanwijit <naruapon@webmail.npru.ac.th>
2026-01-08 17:21:14 +0700

    add route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/66', function () {
     return view('664259027');
 });
+
+Route::get('/55', function () {
+    return view('5510096596');
+});
+
+
